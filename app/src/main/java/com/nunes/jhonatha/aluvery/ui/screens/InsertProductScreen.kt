06ddd2f4090f085e5312daf635fcbe4490d34b44
JhonathaNunes <jhonathasilveira@gmail.com>
2025-02-13 21:32:27 -0300

    Add image preview, click action and scroll in virtual keyboard

--- app/src/main/java/com/nunes/jhonatha/aluvery/ui/screens/InsertProductScreen.kt
+++ app/src/main/java/com/nunes/jhonatha/aluvery/ui/screens/InsertProductScreen.kt
@@ -1,12 +1,18 @@
 package com.nunes.jhonatha.aluvery.ui.screens
 
+import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
@@ -18,19 +24,26 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
 import com.nunes.jhonatha.aluvery.R
+import com.nunes.jhonatha.aluvery.models.Product
 import com.nunes.jhonatha.aluvery.ui.components.FormTextField
 import com.nunes.jhonatha.aluvery.ui.theme.AluveryTheme
+import java.math.BigDecimal
 
 @Composable
 fun InsertProductScreen() {
     Column(
         Modifier
             .fillMaxSize()
+            .imePadding()
+            .verticalScroll(rememberScrollState())
             .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
@@ -38,6 +51,18 @@ fun InsertProductScreen() {
 
         var url by remember {
             mutableStateOf("")
+        }
+
+        AnimatedVisibility(url.isNotBlank()) {
+            AsyncImage(
+                model = url,
+                contentDescription = "Image preview",
+                Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                placeholder = painterResource(R.drawable.placeholder),
+                contentScale = ContentScale.Crop
+            )
         }
 
         FormTextField(
@@ -82,7 +107,22 @@ fun InsertProductScreen() {
         }
 
         Button(
-            onClick = {},
+            onClick = {
+                val priceConverted = try {
+                    BigDecimal(price)
+                } catch (e: NumberFormatException) {
+                    BigDecimal.ZERO
+                }
+
+                val product = Product(
+                    name = name,
+                    price = priceConverted,
+                    image = url,
+                    description = description
+                )
+
+                Log.i("InsertProductScreen", "InsertProductScreen: $product")
+            },
             Modifier.fillMaxWidth(),
             shape = RoundedCornerShape(4),
             elevation = ButtonDefaults.elevatedButtonElevation(10.dp)
