42f751900ce39d159764e7b1157a7e45581853b5
JhonathaNunes <jhonathasilveira@gmail.com>
2025-02-19 21:33:31 -0300

    Add float button

--- app/src/main/java/com/nunes/jhonatha/aluvery/activities/MainActivity.kt
+++ app/src/main/java/com/nunes/jhonatha/aluvery/activities/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.nunes.jhonatha.aluvery.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -18,15 +23,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            App()
+            App {
+                startActivity(Intent(
+                    this,
+                    InsertProductFormActivity::class.java
+                ))
+            }
         }
     }
 }
 
 @Composable
-fun App() {
+fun App(
+    onFloatingButtonClick: () -> Unit
+) {
     AluveryTheme {
-        Scaffold { innerPadding ->
+        Scaffold(
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = onFloatingButtonClick
+                ) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = "Ícone adição")
+                }
+            }
+        ) { innerPadding ->
             Surface(Modifier.padding(innerPadding)) {
                 HomeScreen(sampleSections)
 //            AllProductsScreen(sampleProducts)
