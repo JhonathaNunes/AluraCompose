161b93157350894aba73c4326ee2f73efb2ca2c9
JhonathaNunes <jhonathasilveira@gmail.com>
2025-02-20 09:39:14 -0300

    Remove unused import in MainActivity

--- app/src/main/java/com/nunes/jhonatha/aluvery/activities/MainActivity.kt
+++ app/src/main/java/com/nunes/jhonatha/aluvery/activities/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
